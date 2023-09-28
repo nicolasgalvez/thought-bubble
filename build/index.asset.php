7e025824059339fc0a12be611a0e18170d8a187b
Nick Galvez <nicholas.galvez@gmail.com>
2023-09-27 17:18:48 -0700

    Add a build step

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '6999ec8d63fd7c2434cb');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'c04e1d39a0577d30a9df');
