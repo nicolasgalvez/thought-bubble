bfd9a954b6c8fb8f680f29cb673fa6218022178a
Nick Galvez <nicholas.galvez@gmail.com>
2023-09-27 19:20:20 -0700

    Try building a release on tag

--- thought-bubble.php
+++ thought-bubble.php
@@ -4,7 +4,7 @@
  * Description:       A block that displays a CTA inside the hero cover block
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.2
+ * Version:           0.1.3
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
