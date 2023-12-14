bafeae927c83d608ab5d45dbbf0ca8337648f446
Nick Galvez <nicholas.galvez@gmail.com>
2023-12-13 17:31:36 -0800

    Delete temp zip in github actions, bump version

--- thought-bubble.php
+++ thought-bubble.php
@@ -4,7 +4,7 @@
  * Description:       A block that displays a CTA at the bottom of the page.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.4
+ * Version:           0.1.6
  * Author:            Nick Galvez
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
